00a499a4bdc28a7a5f34cfe1fb089923e53151d4
Peggy Li <xpeggyxli@gmail.com>
2018-04-12 01:17:05 -0400

    Add randomized mole pop-ups

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
+
 var seconds_left = 60;
 var time_interval = setInterval(function() {
 	$("#time-left").text(--seconds_left);
 	if (seconds_left == 0) {
 		clearInterval(time_interval);
+		clearInterval(mole_interval);
 		alert("Game over");
 	}
 }, 1000);
 
-var test_score = 0;
-var score_interval = setInterval(function() {
-	$("#score").text(++test_score);
+var mole_array = $(".mole");
+var mole_interval = setInterval(function() {
+	$.each(mole_array, function() {
+		if (Math.floor(Math.random() * 2) == 1)
+			$(this).toggleClass("mole-hide");
+	});
 }, 1000);
 
 
 $(".mole").click(function() {
 	$(this).attr("src","moles/student1-whacked.png");
-	$(this).css("transform", "translate3d(0,150px,0)");
+	$(this).addClass("mole-hide");
 	var score = parseInt($("#score").text());
 	$("#score").text(score+10);
 });
