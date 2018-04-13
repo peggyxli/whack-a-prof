743493711bc52401be3c1a1dca97fa33760f22f8
Peggy Li <xpeggyxli@gmail.com>
2018-04-13 15:19:14 -0400

    Fix bug and add whacked check

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var mole_types = ["student1", "student2", "professor1", "professor2", "dean1", "
 
 var mole_interval = setInterval(function() {
 	$.each(mole_slots, function(slot_index, value) {
-		if (Math.floor(Math.random() * 2) == 1) {
+		if (Math.floor(Math.random() * 2) == 1 && $(this).not(":animated")) {
 			if ($(this).hasClass("mole-hide")) {
 				var random_index = Math.floor(Math.random() * mole_types.length);
 				$(this).attr("src","moles/" + mole_types[random_index] + ".png");
@@ -28,9 +28,12 @@ var mole_interval = setInterval(function() {
 
 
 $(".mole").click(function() {
-	var image_url = $(this).attr("src").replace(".png", "-whacked.png");
-	$(this).attr("src",image_url);
-	$(this).addClass("mole-hide");
-	var score = parseInt($("#score").text());
-	$("#score").text(score+10);
+	var image_url = $(this).attr("src");
+	if (image_url.search("whacked") == -1) {
+		image_url = image_url.replace(".png", "-whacked.png");
+		$(this).attr("src",image_url);
+		$(this).addClass("mole-hide");
+		var score = parseInt($("#score").text());
+		$("#score").text(score+10);
+	}
 });
