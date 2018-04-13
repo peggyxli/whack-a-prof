8ce6b9a88452491ceef6b08e5920dcc729d4bb7c
Peggy Li <xpeggyxli@gmail.com>
2018-04-12 22:04:22 -0400

    Integrate all mole types

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ var time_interval = setInterval(function() {
 	}
 }, 1000);
 
+var mole_slots = $(".mole");
+var mole_types = ["student1", "student2", "professor1", "professor2", "dean1", "dean2", "trustee"];
 
-var mole_array = $(".mole");
 var mole_interval = setInterval(function() {
-	$.each(mole_array, function() {
+	$.each(mole_slots, function(slot_index, value) {
 		if (Math.floor(Math.random() * 2) == 1) {
 			if ($(this).hasClass("mole-hide")) {
-				$(this).attr("src","moles/student1.png");
+				var random_index = Math.floor(Math.random() * mole_types.length);
+				$(this).attr("src","moles/" + mole_types[random_index] + ".png");
 				$(this).removeClass("mole-hide");
 			}
 			else {
@@ -26,7 +28,8 @@ var mole_interval = setInterval(function() {
 
 
 $(".mole").click(function() {
-	$(this).attr("src","moles/student1-whacked.png");
+	var image_url = $(this).attr("src").replace(".png", "-whacked.png");
+	$(this).attr("src",image_url);
 	$(this).addClass("mole-hide");
 	var score = parseInt($("#score").text());
 	$("#score").text(score+10);
